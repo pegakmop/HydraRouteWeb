f46dfd1a1780ea9ea68bc08f81d5b8fd5152e4b8
Ground-Zerro <ksa65s571dozor@gmail.com>
2025-02-02 04:10:06 +1100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,61 +7,58 @@ VERSION=$(ndmc -c show version | grep "title" | awk -F": " '{print $2}')
 REQUIRED_VERSION="4.2.3"
 ## анимация
 animation() {
-  local pid=$1
-  local message=$2
-  local spin='-\|/'
+	local pid=$1
+	local message=$2
+	local spin='-\|/'
 
-  echo -n "$message... "
+	echo -n "$message... "
 
-  while kill -0 $pid 2>/dev/null; do
-    for i in $(seq 0 3); do
-      echo -ne "\b${spin:$i:1}"
-      usleep 100000  # 0.1 сек
-    done
-  done
+	while kill -0 $pid 2>/dev/null; do
+		for i in $(seq 0 3); do
+			echo -ne "\b${spin:$i:1}"
+			usleep 100000  # 0.1 сек
+		done
+	done
 
   echo -e "\b✔ Готово!"
 }
 
 # удаление пакетов
 opkg_uninstall() {
-  /opt/etc/init.d/S99adguardhome kill
-  /opt/etc/init.d/S99hpanel kill
-  PACKAGES="adguardhome-go ipset iptables node-npm node tar"
-  for pkg in $PACKAGES; do
-    opkg remove "$pkg"
-  done
+	/opt/etc/init.d/S99adguardhome kill
+	/opt/etc/init.d/S99hpanel kill
+	opkg remove adguardhome-go ipset iptables node-npm node tar
 }
 
 # удаление файлов
 files_uninstall() {
-  chmod -R 777 /opt/etc/AdGuardHome/
-  chmod 777 /opt/etc/init.d/S52ipset
-  chmod 777 /opt/var/log/AdGuardHome.log
-  rm -f /opt/etc/ndm/ifstatechanged.d/010-bypass-table.sh
-  rm -f /opt/etc/ndm/ifstatechanged.d/011-bypass6-table.sh
-  rm -f /opt/etc/ndm/netfilter.d/010-bypass.sh
-  rm -f /opt/etc/ndm/netfilter.d/011-bypass6.sh
-  rm -f /opt/etc/init.d/S52ipset
-  rm -rf /opt/etc/AdGuardHome/
-  rm -f /opt/var/log/AdGuardHome.log
+	chmod -R 777 /opt/etc/AdGuardHome/
+	chmod 777 /opt/etc/init.d/S52ipset
+	chmod 777 /opt/var/log/AdGuardHome.log
+	rm -f /opt/etc/ndm/ifstatechanged.d/010-bypass-table.sh
+	rm -f /opt/etc/ndm/ifstatechanged.d/011-bypass6-table.sh
+	rm -f /opt/etc/ndm/netfilter.d/010-bypass.sh
+	rm -f /opt/etc/ndm/netfilter.d/011-bypass6.sh
+	rm -f /opt/etc/init.d/S52ipset
+	rm -rf /opt/etc/AdGuardHome/
+	rm -f /opt/var/log/AdGuardHome.log
 }
 
 # удаление веб-панели
 files_hpanel_uninstall() {
-  chmod -R 777 /opt/etc/HydraRoute/
-  chmod 777 /opt/etc/init.d/S99hpanel
-  rm -rf /opt/etc/HydraRoute/
-  rm -r /opt/etc/init.d/S99hpanel
+	chmod -R 777 /opt/etc/HydraRoute/
+	chmod 777 /opt/etc/init.d/S99hpanel
+	rm -rf /opt/etc/HydraRoute/
+	rm -r /opt/etc/init.d/S99hpanel
 }
 
 # проверка версии прошивки
 firmware_check() {
-  if [ "$(printf '%s\n' "$VERSION" "$REQUIRED_VERSION" | sort -V | tail -n1)" = "$VERSION" ]; then
-      dns_on >>"$LOG" 2>&1 &
-  else
-      dns_on_sh
-  fi
+	if [ "$(printf '%s\n' "$VERSION" "$REQUIRED_VERSION" | sort -V | tail -n1)" = "$VERSION" ]; then
+		dns_on >>"$LOG" 2>&1 &
+	else
+		dns_on_sh
+	fi
 }
 
 # включение системного DNS
@@ -74,10 +71,10 @@ dns_on() {
 # включение системного DNS через "nohup"
 dns_on_sh() {
 	opkg install coreutils-nohup >>"$LOG" 2>&1
-  echo "Удаление завершено (╥_╥)"
+	echo "Удаление завершено (╥_╥)"
 	echo "Включение системного DNS..."
-  echo "Перезагрузка..."
-  /opt/bin/nohup sh -c "ndmc -c 'opkg no dns-override' && ndmc -c 'system configuration save' && sleep 3 && reboot" >>"$LOG" 2>&1
+	echo "Перезагрузка..."
+	/opt/bin/nohup sh -c "ndmc -c 'opkg no dns-override' && ndmc -c 'system configuration save' && sleep 3 && reboot" >>"$LOG" 2>&1
 }
 
 opkg_uninstall >>"$LOG" 2>&1 &
